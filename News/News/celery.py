743f0dd5e8c98db5a3927427debcb8fb26267741
ываппарпвпрвпывыа <stds58@gmail.com>
2024-01-25 23:38:34 +0300

    Initial commit

--- News/News/celery.py
+++ News/News/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdonalds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
 
-app = Celery('mcdonalds')
+app = Celery('news')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
