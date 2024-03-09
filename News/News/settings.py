76fbd292f89b3fb9c8d9ad90b442ede6f6dfbc89
ываппарпвпрвпывыа <stds58@gmail.com>
2024-03-09 20:20:13 +0300

    Initial commit

--- News/News/settings.py
+++ News/News/settings.py
@@ -150,6 +150,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+LANGUAGES = [
+    ('ru','русский'),
+    ('sq','албанский')
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
--- News/News/urls.py
+++ News/News/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', include('sajt.urls')),
